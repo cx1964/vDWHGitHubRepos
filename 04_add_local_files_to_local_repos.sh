e53dc683c6133d067282842dfb8696f1fde7b54c
cx1964 <cx1964@gmail.com>
2018-11-28 14:19:08 +0100

    Voeg een lege structuur toe aan de respository vDWHGitHubRepos

--- 04_add_local_files_to_local_repos.sh
+++ 04_add_local_files_to_local_repos.sh
@@ -1,6 +1,8 @@
 # Filenaam: 04_add_local_files_to_local_repos.sh
 # functie: Voeg alle local files toe aan een local repository
 
-git add ./*
-git add REDAME.md
+git add ./API/*
+git add ./ETL/*
+git add ./PowerBI/*
+git add README.md
 git commit -m "Voeg een lege structuur toe aan de respository vDWHGitHubRepos"
--- 08_save_all_files_to_central_repository.sh
+++ 08_save_all_files_to_central_repository.sh
@@ -1,7 +1,7 @@
 # Filenaam: 08_save_all_files_to_central_repository.sh
 # Functie: Schrijf alle files weg naar de centrale repository
 
-git remote add origin https://github.com/cx1964/_vDWHGitHubRepos.git
+git remote add origin https://github.com/cx1964/vDWHGitHubRepos.git
 
 # Gebruik username cx1964@gmail.com
 
